77d70cf18569a836fb70759e1a72f3e288f8ce2c
kevin <kds300@psu.edu>
2025-01-20 13:12:56 -0500

    added tests directory with unit test for SpikeInputLayer

---
+++ tests/test_io.py
@@ -0,0 +1,38 @@
+"""Unit Tests for IO Module."""
+
+import unittest
+
+import numpy as np
+from lava.magma.core.run_configs import Loihi2SimCfg
+from lava.magma.core.run_conditions import RunSteps
+from lava.proc.io.sink import RingBuffer as Sink
+
+from stmnet.io import SpikeInputLayer
+
+
+class TestSpikeInputLayer(unittest.TestCase):
+    """Unit Tests for the SpikeInputLayer hierarchical process."""
+    def test_cpu_execution(self):
+        """Test that SpikeInputLayer behaves correctly for CPU execution.
+        """
+        N = 5
+        data = np.eye(N)
+
+        source = SpikeInputLayer(data=data)
+        sink = Sink(shape=(N,), buffer=N)
+        source.s_out.connect(sink.a_in)
+
+        source.run(
+            condition=RunSteps(num_steps=N),
+            run_cfg=Loihi2SimCfg()
+        )
+
+        out_data = sink.data.get()
+
+        source.stop()
+
+        self.assertTrue(np.array_equal(data, out_data))
+
+
+if __name__ == "__main__":
+    unittest.main()
