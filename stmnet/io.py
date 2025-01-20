5e3f0640937f2ec64d4d05b20c0c46eda8cb8081
kevin <kds300@psu.edu>
2025-01-20 13:09:54 -0500

    added SpikeInputLayer process and model

--- stmnet/io.py
+++ stmnet/io.py
@@ -3,7 +3,47 @@
 
 import numpy as np
 
+from lava.magma.core.process.process import AbstractProcess
+from lava.magma.core.process.variable import Var
+from lava.magma.core.process.ports.ports import InPort, OutPort
+from lava.proc.io.source import RingBuffer as Source
+from lava.magma.core.model.sub.model import AbstractSubProcessModel
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+from lava.magma.core.decorator import implements
+
 import stmnet.proc as proc
+
+
+class SpikeInputLayer(AbstractProcess):
+    """Creates a spike generating layer to serve as input to a network."""
+    def __init__(self, data: np.ndarray, **kwargs):
+        """
+        Parameters
+        ----------
+        data: array
+            binary array representing spikes.
+            Expects shape (n_ports, n_timesteps)
+        """
+        shape = (data.shape[0],)
+        super().__init__(data=data, shape=shape, **kwargs)
+
+        self.s_out = OutPort(shape=shape)
+
+        self.data = Var(shape=data.shape, init=data)
+
+
+@implements(proc=SpikeInputLayer, protocol=LoihiProtocol)
+class SubSpikeInputLayerModel(AbstractSubProcessModel):
+    """Implements the SpikeInputLayer."""
+    def __init__(self, proc):
+        """Builds spike source using a RingBuffer."""
+        data = proc.proc_params.get("data")
+
+        self.source = Source(data=data)
+
+        self.source.out_ports.s_out.connect(proc.out_ports.s_out)
+        
+        proc.vars.data.alias(self.source.vars.data)
 
 
 def create_binary_io_processes(
