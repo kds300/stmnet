81a14828aa887922cbc097438b109a0d0573c0a2
kevin <kds300@psu.edu>
2025-01-20 14:17:50 -0500

    added unit tests for SpikeOutputLayer

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,9 +5,10 @@ import unittest
 import numpy as np
 from lava.magma.core.run_configs import Loihi2SimCfg
 from lava.magma.core.run_conditions import RunSteps
+from lava.proc.io.source import RingBuffer as Source
 from lava.proc.io.sink import RingBuffer as Sink
 
-from stmnet.io import SpikeInputLayer
+from stmnet.io import SpikeInputLayer, SpikeOutputLayer
 
 
 class TestSpikeInputLayer(unittest.TestCase):
@@ -30,6 +31,30 @@ class TestSpikeInputLayer(unittest.TestCase):
         out_data = sink.data.get()
 
         source.stop()
+
+        self.assertTrue(np.array_equal(data, out_data))
+
+
+class TestSpikeOutputLayer(unittest.TestCase):
+    """Unit Tests for the SpikeOutputLayer hierarchical process."""
+    def test_cpu_execution(self):
+        """Test that SpikeOutputLayer behaves correctly for CPU execution.
+        """
+        N = 5
+        data = np.eye(N)
+
+        source = Source(data=data)
+        sink = SpikeOutputLayer(shape=(N,), buffer=N)
+        source.s_out.connect(sink.a_in)
+
+        sink.run(
+            condition=RunSteps(num_steps=N),
+            run_cfg=Loihi2SimCfg()
+        )
+
+        out_data = sink.data.get()
+
+        sink.stop()
 
         self.assertTrue(np.array_equal(data, out_data))
 
